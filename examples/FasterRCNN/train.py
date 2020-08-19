c18bb0620befa091d5c659e43041bde196f133f0
leeshien <ganleeshien@gmail.com>
2020-08-19 22:11:54 +0800

    skip adding PYTHONPATH env

--- examples/FasterRCNN/train.py
+++ examples/FasterRCNN/train.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     # "spawn/forkserver" is safer than the default "fork" method and
     # produce more deterministic behavior & memory saving
     # However its limitation is you cannot pass a lambda function to subprocesses.
-    os.environ['PYTHONPATH'] += ":/tensorpack"
+#     os.environ['PYTHONPATH'] += ":/tensorpack"
     import multiprocessing as mp
     mp.set_start_method('spawn')
     parser = argparse.ArgumentParser()
