a222bd17bbb3542bf40b836832c9d2c9a43090e9
leeshien <ganleeshien@gmail.com>
2020-09-04 17:50:36 +0800

    eval.py - mask -> masks

--- examples/FasterRCNN/eval.py
+++ examples/FasterRCNN/eval.py
@@ -146,7 +146,7 @@ def predict_image(img):
                 boxes = sess.run(output_tensor_boxes, feed_dict={input_image_tensor: resized_img})
                 scores = sess.run(output_tensor_scores, feed_dict={input_image_tensor: resized_img})
                 labels = sess.run(output_tensor_labels, feed_dict={input_image_tensor: resized_img})
-                mask = sess.run(output_tensor_masks, feed_dict={input_image_tensor: resized_img})
+                masks = sess.run(output_tensor_masks, feed_dict={input_image_tensor: resized_img})
     #====================================================================================
 
     # Some slow numpy postprocessing:
