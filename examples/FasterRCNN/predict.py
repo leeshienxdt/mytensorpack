4d1f2f0d42734aedaa084e20a4ad11745c1cfacd
leeshien <ganleeshien@gmail.com>
2020-09-07 10:01:47 +0800

    create session in do_predict

--- examples/FasterRCNN/predict.py
+++ examples/FasterRCNN/predict.py
@@ -94,9 +94,25 @@ def do_evaluate(pred_config, output_file):
 
 
 def do_predict(input_file, output_file):
+    g = tf.Graph().as_default()
+    output_graph_def = tf.compat.v1.GraphDef()
+    with open(pb_path, "rb") as f:
+        output_graph_def.ParseFromString(f.read()) 
+    tf.import_graph_def(output_graph_def, name="")
+    config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
+    sess = tf.compat.v1.Session(config=config)        
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    input_tensor = sess.graph.get_tensor_by_name("image:0")
+    output_tensor_boxes = sess.graph.get_tensor_by_name("output/boxes:0")
+    output_tensor_scores = sess.graph.get_tensor_by_name("output/scores:0")
+    output_tensor_labels = sess.graph.get_tensor_by_name("output/labels:0")
+    output_tensor_masks = sess.graph.get_tensor_by_name("output/masks:0")   
+    output_tensors = [output_tensor_boxes, output_tensor_scores, output_tensor_labels, output_tensor_masks]
+    
     print('input fn: ', input_file)
     img = cv2.imread(input_file, cv2.IMREAD_COLOR)
-    results = predict_image(img)
+    results = predict_image(sess, input_tensor, output_tensors, img)
     if cfg.MODE_MASK:
         final = draw_final_outputs_blackwhite(img, results)
     else:
