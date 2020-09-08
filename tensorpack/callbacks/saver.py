642ac3da30020284affccb5e8de18a13495f79d5
leeshien <ganleeshien@gmail.com>
2020-09-08 16:50:42 +0800

    add return_devices

--- tensorpack/callbacks/saver.py
+++ tensorpack/callbacks/saver.py
@@ -70,7 +70,7 @@ class ModelSaver(Callback):
         self.saver.export_meta_graph(
             os.path.join(self.checkpoint_dir,
                          'graph-{}.meta'.format(time)),
-            collection_list=self.graph.get_all_collection_keys())
+            collection_list=self.graph.get_all_collection_keys(), return_devices=True)
 
     def _trigger(self):
         try:
