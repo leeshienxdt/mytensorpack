53e41761e92314ec8397d91b2eac7a5c39912a33
leeshien <ganleeshien@gmail.com>
2020-09-09 15:31:58 +0800

    Add savepb.py

---
+++ examples/FasterRCNN/savepb.py
@@ -0,0 +1,20 @@
+import argparse
+
+from modeling.generalized_rcnn import ResNetC4Model, ResNetFPNModel
+from tensorpack.predict import PredictConfig
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load', help='load a model for evaluation.', required=True)
+    parser.add_argument('--output-pb', help='Save a model to .pb')
+    args = parser.parse_args()
+
+	MODEL = ResNetFPNModel() if cfg.MODE_FPN else ResNetC4Model()
+
+	predcfg = PredictConfig(
+	    model=MODEL,
+	    session_init=SmartInit(args.load),
+	    input_names=MODEL.get_inference_tensor_names()[0],
+	    output_names=MODEL.get_inference_tensor_names()[1])
+
+	ModelExporter(predcfg).export_compact(args.output_pb, optimize=False)
